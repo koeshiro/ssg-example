64d4ddd1057c1ac61979ccffab65c2e1f9b774d4
Rustam Borkhanov <koeshiro@yandex.ru>
2025-07-10 17:03:18 +0300

    feat: add work with docker

--- backend/main.go
+++ backend/main.go
@@ -12,7 +12,8 @@ import (
 )
 
 type EnvConfigInterface struct {
-	AppGoogleBookApiKey string `required:"true" envconfig:"SSG_EXAMPLE_GOOGLE_BOOK_API_KEY"`
+	AppGoogleBookApiKey string `required:"true" envconfig:"GOOGLE_BOOK_API_KEY"`
+	Port                string `default:"8080" envconfig:"PORT"`
 }
 
 type UrlParams struct {
@@ -34,12 +35,46 @@ func sendErrorMessageIfErrorNotNil(ctx *gin.Context, err error) {
 	throwPanicIfErrorNotNil(err)
 }
 
+func GetBooksList(ctx *gin.Context, booksService *books.Service) {
+	var query string
+	var startIndex int64
+	var maxResults int64
+	var err error
+	if value, ok := ctx.GetQuery("query"); ok {
+		query = value
+	}
+	if value, ok := ctx.GetQuery("startIndex"); ok {
+		startIndex, err = strconv.ParseInt(value, 10, 64)
+		sendErrorMessageIfErrorNotNil(ctx, err)
+	}
+
+	if value, ok := ctx.GetQuery("maxResults"); ok {
+		maxResults, err = strconv.ParseInt(value, 10, 64)
+		sendErrorMessageIfErrorNotNil(ctx, err)
+	}
+	ctx.GetQuery("startIndex")
+	ctx.GetQuery("maxResults")
+	booksList, err := booksService.Volumes.List(query).StartIndex(startIndex).MaxResults(maxResults).Do()
+	sendErrorMessageIfErrorNotNil(ctx, err)
+	ctx.JSON(http.StatusOK, booksList)
+}
+
+func GetBook(ctx *gin.Context, booksService *books.Service) {
+	var urlParams UrlParams
+	err := ctx.ShouldBindUri(&urlParams)
+	sendErrorMessageIfErrorNotNil(ctx, err)
+	book, err := booksService.Volumes.Get(urlParams.ID).Do()
+	sendErrorMessageIfErrorNotNil(ctx, err)
+	ctx.JSON(http.StatusOK, book)
+}
+
 func main() {
 	var envConfig EnvConfigInterface
 	err := envconfig.Process("", &envConfig)
 	throwPanicIfErrorNotNil(err)
 
 	booksService, err := books.NewService(context.Background(), option.WithAPIKey(envConfig.AppGoogleBookApiKey))
+	throwPanicIfErrorNotNil(err)
 
 	var defaultOptions = make(map[string]string)
 	defaultOptions["language"] = "ru"
@@ -47,34 +82,10 @@ func main() {
 	r := gin.Default()
 
 	r.GET("/books", func(ctx *gin.Context) {
-		var query string
-		var startIndex int64
-		var maxResults int64
-		if value, ok := ctx.GetQuery("query"); ok {
-			query = value
-		}
-		if value, ok := ctx.GetQuery("startIndex"); ok {
-			startIndex, err = strconv.ParseInt(value, 10, 64)
-			sendErrorMessageIfErrorNotNil(ctx, err)
-		}
-
-		if value, ok := ctx.GetQuery("maxResults"); ok {
-			maxResults, err = strconv.ParseInt(value, 10, 64)
-			sendErrorMessageIfErrorNotNil(ctx, err)
-		}
-		ctx.GetQuery("startIndex")
-		ctx.GetQuery("maxResults")
-		booksList, err := booksService.Volumes.List(query).StartIndex(startIndex).MaxResults(maxResults).Do()
-		sendErrorMessageIfErrorNotNil(ctx, err)
-		ctx.JSON(http.StatusOK, booksList)
+		GetBooksList(ctx, booksService)
 	})
 	r.GET("/book/:id", func(ctx *gin.Context) {
-		var urlParams UrlParams
-		err := ctx.ShouldBindUri(&urlParams)
-		sendErrorMessageIfErrorNotNil(ctx, err)
-		book, err := booksService.Volumes.Get(urlParams.ID).Do()
-		sendErrorMessageIfErrorNotNil(ctx, err)
-		ctx.JSON(http.StatusOK, book)
+		GetBook(ctx, booksService)
 	})
 	r.GET("/ping", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{
@@ -86,5 +97,5 @@ func main() {
 			"message": "Not found",
 		})
 	})
-	r.Run(":8080")
+	r.Run(":" + envConfig.Port)
 }
