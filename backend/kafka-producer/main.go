025e264b8f78ac8cfbfb676d42d128fa954af9ad
Rustam Borkhanov <koeshiro@yandex.ru>
2025-07-23 13:14:44 +0300

    feat: split connections to kafka, add logs

--- backend/kafka-producer/main.go
+++ backend/kafka-producer/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"strconv"
 
 	"github.com/IBM/sarama"
@@ -22,18 +23,31 @@ type EnvConfigInterface struct {
 func createTopic(topic string, NumPartitions int32, client sarama.Client) error {
 	clusterAdmin, err := sarama.NewClusterAdminFromClient(client)
 	utils.ThrowPanicIfErrorNotNil(err)
+	defer func() {
+		err := clusterAdmin.Close()
+		utils.ThrowPanicIfErrorNotNil(err)
+	}()
+	topics, err := clusterAdmin.ListTopics()
+	utils.ThrowPanicIfErrorNotNil(err)
+
+	if _, ok := topics[topic]; ok {
+		clusterAdmin.DeleteTopic(topic)
+	}
 	topicDetail := &sarama.TopicDetail{
 		NumPartitions:     NumPartitions,
 		ReplicationFactor: 1,
 	}
-	defer clusterAdmin.Close()
-	return clusterAdmin.CreateTopic(topic, topicDetail, true)
+	err = clusterAdmin.CreateTopic(topic, topicDetail, false)
+	return err
 }
 
 func sendData(topic string, client sarama.Client, booksService *books.Service) {
 	producer, err := sarama.NewAsyncProducerFromClient(client)
 	utils.ThrowPanicIfErrorNotNil(err)
-	defer producer.Close()
+	defer func() {
+		err := producer.Close()
+		utils.ThrowPanicIfErrorNotNil(err)
+	}()
 	var subjects []string = []string{
 		"subject:IT",
 		"Golang",
@@ -53,13 +67,15 @@ func sendData(topic string, client sarama.Client, booksService *books.Service) {
 		for currentPage := int64(0); currentPage < 8; currentPage++ {
 			books, err := booksService.Volumes.List(subject).MaxResults(maxResults).StartIndex(currentPage * maxResults).Do()
 			utils.ThrowPanicIfErrorNotNil(err)
-
+			if len(books.Items) == 0 {
+				break
+			}
 			jsonData, err := json.Marshal(books.Items)
 			utils.ThrowPanicIfErrorNotNil(err)
 
 			chanel <- &sarama.ProducerMessage{
 				Topic: topic,
-				Key:   sarama.StringEncoder(subject + strconv.FormatInt(currentPage, 10)),
+				Key:   sarama.StringEncoder(subject + "-" + strconv.FormatInt(currentPage, 10)),
 				Value: sarama.StringEncoder(string(jsonData)),
 			}
 		}
@@ -67,24 +83,33 @@ func sendData(topic string, client sarama.Client, booksService *books.Service) {
 }
 
 func main() {
+	fmt.Println("Read env config")
 	var envConfig EnvConfigInterface
 	err := envconfig.Process("", &envConfig)
 
+	fmt.Println("Create sarama config")
 	config := sarama.NewConfig()
 	utils.ThrowPanicIfErrorNotNil(err)
 
-	client, err := sarama.NewClient([]string{envConfig.KafkaUrl}, config)
+	fmt.Println("Create sarama admin client")
+	clientAdmin, err := sarama.NewClient([]string{envConfig.KafkaUrl}, config)
 	utils.ThrowPanicIfErrorNotNil(err)
 
 	numPartitions, err := strconv.ParseInt(envConfig.KafkaNumPartitions, 10, 32)
 	utils.ThrowPanicIfErrorNotNil(err)
 
-	err = createTopic(envConfig.KafkaTopicName, int32(numPartitions), client)
+	fmt.Println("Create kafka topic")
+	err = createTopic(envConfig.KafkaTopicName, int32(numPartitions), clientAdmin)
 	utils.ThrowPanicIfErrorNotNil(err)
 
+	fmt.Println("Connect to google books")
 	booksService, err := books.NewService(context.Background(), option.WithAPIKey(envConfig.AppGoogleBookApiKey))
 	utils.ThrowPanicIfErrorNotNil(err)
 
-	sendData(envConfig.KafkaTopicName, client, booksService)
+	fmt.Println("Create sarama admin client")
+	clientProvider, err := sarama.NewClient([]string{envConfig.KafkaUrl}, config)
+	utils.ThrowPanicIfErrorNotNil(err)
 
+	fmt.Println("Start sending data to kafka")
+	sendData(envConfig.KafkaTopicName, clientProvider, booksService)
 }
