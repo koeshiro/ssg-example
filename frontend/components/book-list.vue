98f750be727007000c6b2bed9e32ce90d083e4c7
Rustam Borkhanov <koeshiro@yandex.ru>
2025-09-30 00:13:35 +0300

    feat: readme update

--- frontend/components/book-list.vue
+++ frontend/components/book-list.vue
@@ -24,7 +24,13 @@
         />
       </v-col>
       <v-col cols="1">
-        <v-btn icon @click="async () => books = await loadPageBooks(currentSearch, currentPage)">
+        <v-btn icon @click="async () => {
+          const response = await loadPageBooks(currentSearch, currentPage)
+          if (booksData) {
+            booksData.books = response.items
+            booksData.pageCount = totalItemsToPageCount(response.totalItems ?? 0)
+          }
+        }">
           <v-icon>mdi-magnify</v-icon>
         </v-btn>
       </v-col>
@@ -38,7 +44,7 @@
     </template>
     <template v-else>
       <v-row dense>
-        <v-col v-for="(item, index) in books" :key="index" cols="12" sm="6" md="3">
+        <v-col v-for="(item, index) in booksData?.books" :key="index" cols="12" sm="6" md="3">
           <BookListItem
             :title="item.volumeInfo?.title"
             :subtitle="item.volumeInfo?.subtitle"
@@ -49,9 +55,9 @@
       <v-row :key="currentPage">
         <v-col>
           <v-pagination
-            v-if="pageCount > 0"
+            v-if="booksData?.pageCount && booksData?.pageCount > 0"
             v-model="currentPage"
-            :length="pageCount"
+            :length="booksData?.pageCount"
             :total-visible="7"
             class="mt-4"
             rounded color="primary"
@@ -64,6 +70,7 @@
 
 <script async setup lang="ts">
 import type { BooksVolume } from '~/api/backend';
+import { useApi } from '~/composables/api';
 
 const props = withDefaults(
   defineProps<{ search?: string; page?: number, chips?: string[] }>(),
@@ -79,7 +86,6 @@ const maxResults = 40
 const isLoading = ref(false)
 const currentSearch = ref(props.search)
 const currentPage = ref(props.page)
-const pageCount = ref(0)
 const { backend } = useApi()
 
 const totalItemsToPageCount = (totalItems: number) => {
@@ -91,7 +97,11 @@ const totalItemsToPageCount = (totalItems: number) => {
 
 const loadPageBooks = async (search: string, page: number = 1) => {
   if (!search.length) {
-    return [];
+    return {
+      items: [],
+      kind: '',
+      totalItems: 0,
+    }
   }
   const startPage = page - 1 > 0 ? page - 1 : 0
   isLoading.value = true
@@ -101,23 +111,27 @@ const loadPageBooks = async (search: string, page: number = 1) => {
     query: search
   }))
 
-  pageCount.value = totalItemsToPageCount(response.totalItems ?? 0)
   isLoading.value = false
-  return response.items
+  return response
 }
 
 const event = useRequestEvent()
-const { data: books } = await useAsyncData(
-  'books',
-  () => {
+const { data: booksData } = await useAsyncData(
+  'booksData',
+  async () => {
     if (event?.context?.payload?.books) {
-      if (event?.context?.payload?.totalItems) {
-        pageCount.value = totalItemsToPageCount(event?.context?.payload?.totalItems ?? 0)
-      }
-      return Promise.all(event?.context?.payload?.books as BooksVolume[])
+      return Promise.resolve({
+        books: event?.context?.payload?.books as BooksVolume[],
+        pageCount: event?.context?.payload?.totalItems? totalItemsToPageCount(event?.context?.payload?.totalItems ?? 0) : 1
+      })
     }
 
-    return loadPageBooks(currentSearch.value, currentPage.value)
+    const response = await loadPageBooks(currentSearch.value, currentPage.value)
+
+    return {
+      books: response.items,
+      pageCount: totalItemsToPageCount(response.totalItems ?? 0)
+    }
   },
   {
     watch: [
